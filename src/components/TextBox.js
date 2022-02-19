e763e946d9046fd3c06f2df710d8f3f9fdfa437f
Eli Davis <eli.davis42@gmail.com>
2022-02-18 20:08:55 -0500

    Front end setup

---
+++ src/components/TextBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+const TextBox = () => {
+    return (
+        <div>
+            <h1>This will be the  wisdom</h1>
+        </div>
+    )
+}
+
+
+
+export default TextBox;
