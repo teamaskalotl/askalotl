ead2d7d93ad48d33b506cedb5e464607696efb98
samhcarrasco <samuelcarrasco@Samuels-Air.fios-router.home>
2022-02-19 16:11:54 -0500

    User Sign Up

---
+++ server/controllers/user_controller.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const { ContextExclusionPlugin } = require('webpack')
+const db = require(path.join(__dirname, '../models/axolotl-models'))
+
+const user_controller = {};
+
+user_controller.addUser = (req, res, next) => {
+    const query = `INSERT INTO users (username, password, name, sign, vibe)
+                   VALUES ($1, $2, $3, $4, $5)
+                   RETURNING name`
+    const { username, password, name, sign, vibe } = req.body;
+
+    db.query(query, [username, password, name, sign, vibe])
+        .then(data => {
+            res.locals.name = data.rows[0].name;
+            return next()
+        })
+        .catch((e) => {
+            return next({
+                log: "Error occcurred in user_controller.addUser",
+                message: { e: "An error occurred" }
+            })
+        })
+}
+
+user_controller.getUser = (req, res, next) => {
+    const query = `SELECT name FROM users WHERE username = $1 AND password = $2`;
+    const { username, password } = req.params;
+
+    db.query(query, [username, password])
+        .then(data => {
+            res.locals.name = data.rows[0].name;
+            return next()
+        })
+        .catch((e) => {
+            return next({
+                log: "Error occcurred in user_controller.getUser",
+                message: { e: "An error occurred" }
+            })
+        })
+}
+
+module.exports = user_controller;
