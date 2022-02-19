3acfc5390d0a47f8b75cc533f5f55ed6f91f8800
Eric Lemay <ericlemay@Erics-Air.lan>
2022-02-19 11:34:17 -0500

    adds functionality to button

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-import render from 'react-dom'
-import Button from '../components/Button.js'
-import TextBox from '../components/TextBox.js'
+import render from 'react-dom';
+import Button from '../components/Button.js';
+import TextBox from '../components/TextBox.js';
+import Wizard from '../assets/axolotlwizard.png';
 
 class MainContainer extends Component {
-  constructor() {
-    super();
-    this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      wisdomStatus: 'notFetched',
+      wisdom: '',
+    };
+    this.fetchWisdom = this.fetchWisdom.bind(this);
+  }
+  fetchWisdom() {
+    //get request
+    this.setState({ wisdomStatus: 'fetched', wisdom: 'This is the wisdom' });
   }
   render() {
+    const { wisdom } = this.state;
+
+    const renderWisdom = () => {
+      if (this.state.wisdomStatus == 'fetched') {
+        return <TextBox wisdom={wisdom} />;
+      } else return <h1>Awaiting your command</h1>;
+    };
+
     return (
-      <div>
-        <Button/>
-        <TextBox/>
+      <div className="MainContainer">
+        <img className="Wizard" src={Wizard} />
+        <Button fetchWisdom={this.fetchWisdom} />
+        {renderWisdom()}
       </div>
-    )
-  };
+    );
+  }
 }
-  
+
 export default MainContainer;
