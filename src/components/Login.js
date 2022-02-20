f5b9c06a0ab84a9cb99816f124d48065bcb00d82
Eli Davis <eli.davis42@gmail.com>
2022-02-20 15:11:43 -0500

    restored stash

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,56 @@
 import React, { Component } from 'react';
 
-const Login = (props) => {
-    
+class Login extends Component {
+    initialState = {
+        name: '',
+        username: '',
+        password: '',
+        toggleSignUp: false,
+    }
+    state = this.initialState;
 
-  return (
-    <div>
-        <form onSubmit={() => props.login()}>
-          <label>
-            Email or username
-            <input
-              name="emailOrUsername"
-              type="text"
-            //   value={this.state.emailOrUsername}
-            //   onChange={this.handleInputChange}
-            />
-          </label>
-          <label>
-            Password
-            <input
-              name="password"
-              type="password"
-            //   value={this.state.password}
-            //   onChange={this.handleInputChange}
-            />
-          </label>
-          <button type="submit">Log in</button>
-        </form>
-      <button className='loginButton' onClick={() => props.login()}>
-        Login
-      </button>
-    </div>
-  );
-};
+    logOrSign () {
+        if (toggleSignUp) {
+
+        }
+        else {
+            
+
+        }
+
+    }
+
+render () {
+    return (
+        <div>
+            <form onSubmit={() => props.login()}>
+              <label>
+                Email or username
+                <input
+                  name="emailOrUsername"
+                  type="text"
+                value={this.state.username}
+                onChange={this.handleInputChange}
+                />
+              </label>
+              <label>
+                Password
+                <input
+                  name="password"
+                  type="password"
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                />
+              </label>
+              <button type="submit">Log in</button>
+            </form>
+          <button className='loginButton' onClick={() => props.login()}>
+            Login
+          </button>
+        </div>
+      );
+    }
+}
+  
 
 export default Login;
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 const TextBox = (props) => {
+    const renderWisdom = () => {
+        if (this.state.wisdomStatus == 'fetched') {
+        return <TextBox wisdom={wisdom} />;
+        } else return <h1>Awaiting your command</h1>;
+      }
   return (
     <div>
-      <h1>{props.wisdom}</h1>
+      <h1>{renderWisdom()}</h1>
     </div>
   );
 };
